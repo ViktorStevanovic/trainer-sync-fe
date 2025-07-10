a1dde73fd6f84c3f9700b64005f867f96f89466c
ViktorStevanovic <stevanovic.lavoro@gmail.com>
2025-07-10 19:37:44 +0200

    fix

--- src/workarea/users/views/UsersListView.vue
+++ src/workarea/users/views/UsersListView.vue
@@ -59,7 +59,7 @@ onMounted(fetchUsers)
                             <th
                                 class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                             >
-                                Actions
+                                Action
                             </th>
                         </tr>
                     </thead>
